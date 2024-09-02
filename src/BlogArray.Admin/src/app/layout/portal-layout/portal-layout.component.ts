436a701aebb5784dbde320722401a96b075ae671
Sisindry Medagam <24821639+sisindrymedagam@users.noreply.github.com>
2024-09-02 22:39:37 +0530

    ui

--- src/BlogArray.Admin/src/app/layout/portal-layout/portal-layout.component.ts
+++ src/BlogArray.Admin/src/app/layout/portal-layout/portal-layout.component.ts
@@ -7,6 +7,88 @@ import { DropdownComponent } from '../../shared/ui/dropdown/dropdown.component';
   styleUrl: './portal-layout.component.css'
 })
 export class PortalLayoutComponent implements OnInit {
+
+  menuItems = [
+    {
+      name: 'Dashboard',
+      link: 'dashboard',
+      icon: 'chart-pie',
+      items: []
+    },
+    {
+      name: 'Posts',
+      icon: 'pencil',
+      items: [{
+        name: 'All posts',
+        link: 'posts',
+      },
+      {
+        name: 'Add new post',
+        link: 'post/new',
+      }]
+    },
+    {
+      name: 'Pages',
+      icon: 'pencil',
+      items: [{
+        name: 'All pages',
+        link: 'pages',
+      },
+      {
+        name: 'Add new page',
+        link: 'page/new',
+      }]
+    },
+    {
+      name: 'Categories',
+      icon: 'squares-plus',
+      items: [{
+        name: 'All categories',
+        link: 'categories',
+      },
+      {
+        name: 'Add new category',
+        link: 'categories/new',
+      }]
+    },
+    {
+      name: 'Media',
+      icon: 'photo',
+      items: [{
+        name: 'Library',
+        link: 'media',
+      },
+      {
+        name: 'Add new media',
+        link: 'media/new',
+      }]
+    },
+    {
+      name: 'Settings',
+      icon: 'cog-6-tooth',
+      items: [{
+        name: 'General',
+        link: 'pages',
+      },
+      {
+        name: 'Email',
+        link: 'page/new',
+      },
+      {
+        name: 'Email',
+        link: 'page/new',
+      },
+      {
+        name: 'Email',
+        link: 'page/new',
+      },
+      {
+        name: 'Email',
+        link: 'page/new',
+      }]
+    },
+  ];
+
   ngOnInit(): void {
 
   }
