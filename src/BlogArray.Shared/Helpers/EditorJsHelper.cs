96dbe176d56c5fc0603f1e62233cdf31732f126f
Sisindry Medagam <24821639+sisindrymedagam@users.noreply.github.com>
2024-10-13 23:24:43 +0530

    editorjs improvements

--- src/BlogArray.Shared/Helpers/EditorJsHelper.cs
+++ src/BlogArray.Shared/Helpers/EditorJsHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Text;
+using System.Text.Json;
 
 namespace BlogArray.Shared.Helpers;
 
@@ -11,19 +12,19 @@ public static class EditorJsHelper
         {
             dynamic? blocks = JsonSerializer.Deserialize<dynamic?>(editorJsJson);
 
-            if (blocks == null)
+            if (blocks?.blocks == null)
             {
                 return editorJsJson;
             }
 
-            string buildHtml = string.Empty;
+            StringBuilder buildHtml = new();
 
             foreach (dynamic block in blocks.blocks)
             {
-                buildHtml += GetHtmlForBlock(block);
+                buildHtml.Append(GetHtmlForBlock(block));
             }
 
-            return buildHtml;
+            return buildHtml.ToString();
         }
         catch (Exception)
         {
@@ -33,94 +34,110 @@ public static class EditorJsHelper
 
     private static string GetHtmlForBlock(dynamic block)
     {
-        string html = string.Empty;
-        switch (block.type.ToString())
-        {
-            case "header":
-                html = ParseHeader(block);
-                break;
-            case "raw":
-                html = ParseRaw(block);
-                break;
-            case "paragraph":
-                html = ParseParagraph(block);
-                break;
-            case "delimiter":
-                html = ParseDivider();
-                break;
-            case "embed":
-                html = ParseEmbed(block);
-                break;
-            case "image":
-                html = ParseImage(block);
-                break;
-            case "list":
-                html += ParseList(block);
-                break;
-            case "codeblock":
-                html = ParseCode(block);
-                break;
-            case "quote":
-                html = ParseQuote(block);
-                break;
-            case "linkTool":
-                html = ParseLink(block);
-                break;
-            case "table":
-                html = ParseTable(block);
-                break;
-            default:
-                break;
-        }
+        //string html = string.Empty;
+        //switch (block.type.ToString())
+        //{
+        //    case "header":
+        //        html = ParseHeader(block);
+        //        break;
+        //    case "raw":
+        //        html = ParseRaw(block);
+        //        break;
+        //    case "paragraph":
+        //        html = ParseParagraph(block);
+        //        break;
+        //    case "delimiter":
+        //        html = ParseDivider();
+        //        break;
+        //    case "embed":
+        //        html = ParseEmbed(block);
+        //        break;
+        //    case "image":
+        //        html = ParseImage(block);
+        //        break;
+        //    case "list":
+        //        html += ParseList(block);
+        //        break;
+        //    case "codeblock":
+        //        html = ParseCode(block);
+        //        break;
+        //    case "quote":
+        //        html = ParseQuote(block);
+        //        break;
+        //    case "linkTool":
+        //        html = ParseLink(block);
+        //        break;
+        //    case "table":
+        //        html = ParseTable(block);
+        //        break;
+        //    default:
+        //        break;
+        //}
 
-        return html;
+        //return html;
+
+        return block.type.ToString() switch
+        {
+            "header" => ParseHeader(block),
+            "raw" => ParseRaw(block),
+            "paragraph" => ParseParagraph(block),
+            "delimiter" => ParseDivider(),
+            "embed" => ParseEmbed(block),
+            "image" => ParseImage(block),
+            "list" => ParseList(block),
+            "codeblock" => ParseCode(block),
+            "quote" => ParseQuote(block),
+            "linkTool" => ParseLink(block),
+            "table" => ParseTable(block),
+            _ => string.Empty,
+        };
     }
 
     private static string ParseTable(dynamic block)
     {
-        string header = string.Empty, body = string.Empty;
-        bool hadHeaders = Converter.ToBoolean(block.data.withHeadings);
+        var tableHtml = new StringBuilder();
+        var hadHeaders = Converter.ToBoolean(block.data.withHeadings);
 
         if (hadHeaders)
         {
-            dynamic hdrArr = block.data.content[0];
-            header += "<tr>";
-            foreach (dynamic head in hdrArr)
+            var headerRow = block.data.content[0];
+            tableHtml.Append("<thead><tr>");
+            foreach (var head in headerRow)
             {
-                header += $"<th>{Converter.ToString(head)}</th>";
+                tableHtml.Append($"<th>{Converter.ToString(head)}</th>");
             }
-            header += "<tr>";
+            tableHtml.Append("</tr></thead>");
         }
 
-        int index = hadHeaders ? 1 : 0;
-
-        for (int i = index; i < block.data.content.Count; i++)
+        tableHtml.Append("<tbody>");
+        var startRow = hadHeaders ? 1 : 0;
+        for (int i = startRow; i < block.data.content.Count; i++)
         {
-            dynamic cblock = block.data.content[i];
-            body += "<tr>";
-            foreach (dynamic v in cblock)
+            var row = block.data.content[i];
+            tableHtml.Append("<tr>");
+            foreach (var cell in row)
             {
-                body += $"<td>{Converter.ToString(v)}</td>";
+                tableHtml.Append($"<td>{Converter.ToString(cell)}</td>");
             }
-            body += "</tr>";
+            tableHtml.Append("</tr>");
         }
+        tableHtml.Append("</tbody>");
 
-        return $"<div class='table-responsive'><table class='table table-bordered'><thead>{header}</thead><tbody>{body}</tbody></table></div>";
+        return $"<div class='table-responsive'><table class='table table-bordered'>{tableHtml}</table></div>";
     }
 
     private static string ParseHeader(dynamic block)
     {
-        string alignment = "start";
-        if (block.data.alignment == "center")
+        var level = Converter.ToString(block.data.level);
+        var alignment = block.data.alignment.ToString() switch
         {
-            alignment = "center";
-        }
-        else if (block.data.alignment == "right")
-        {
-            alignment = "end";
-        }
+            "center" => "center",
+            "right" => "end",
+            _ => "start"
+        };
+        var text = Converter.ToString(block.data.text);
 
-        return $"<h{Converter.ToString(block.data.level)} class='text-{alignment}'>{Converter.ToString(block.data.text)}</h{Converter.ToString(block.data.level)}>";
+        return $"<h{level} class='text-{alignment}'>{text}</h{level}>";
     }
 
     private static string ParseParagraph(dynamic block)
@@ -130,87 +147,80 @@ public static class EditorJsHelper
 
     private static string ParseRaw(dynamic block)
     {
-        return $"{Converter.ToString(block.data.html)}";
+        return Converter.ToString(block.data.html);
     }
-
 
     private static string ParseEmbed(dynamic block)
     {
+        var embedUrl = Converter.ToString(block.data.embed);
+        var caption = Converter.ToString(block.data.caption);
+
         return $"<figure class='figure'>" +
-            $"<iframe width='560' height='420' class='figure-img img-fluid' src='{Converter.ToString(block.data.embed)}' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>" +
-            $"<figcaption class='figure-caption'>{Converter.ToString(block.data.caption)}</figcaption>" +
+            $"<iframe width='560' height='420' class='figure-img img-fluid' src='{embedUrl}' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>" +
+            $"<figcaption class='figure-caption'>{caption}</figcaption>" +
             $"</figure>";
     }
 
     private static string ParseImage(dynamic block)
     {
+        var imageUrl = Converter.ToString(block.data.url);
+        var caption = Converter.ToString(block.data.caption);
+
         return $"<figure class='figure'>" +
-            $"<img src='{Converter.ToString(block.data.url)}' class='figure-img img-fluid' alt='{Converter.ToString(block.data.caption)}'>" +
-            $"<figcaption class='figure-caption'>{Converter.ToString(block.data.caption)}</figcaption>" +
+            $"<img src='{imageUrl}' class='figure-img img-fluid' alt='{caption}'>" +
+            $"<figcaption class='figure-caption'>{caption}</figcaption>" +
             $"</figure>";
     }
 
     private static string ParseList(dynamic block)
     {
-        string listStyle = Converter.ToString(block.data?.style) == "unordered" ? "ul" : "ol";
-        //var html = "<" + listStyle + ">";
-        //foreach (var li in block.data.items)
-        //{
-        //    html += $"<li>{Converter.ToString(li)}</li>";
-        //};
-        //html += "</" + listStyle + ">";
-
-        return ListMaker(block.data.items, listStyle);
+        var listType = Converter.ToString(block.data?.style) == "unordered" ? "ul" : "ol";
+        return BuildListItems(block.data.items, listType);
     }
 
-    private static string ListMaker(dynamic items, string style)
+    private static string BuildListItems(dynamic items, string listType)
     {
-        string element = "";
+        var listHtml = new StringBuilder();
+        listHtml.Append($"<{listType}>");
 
-        foreach (dynamic item in items)
+        foreach (var item in items)
         {
-            if (string.IsNullOrEmpty(Converter.ToString(item.content)) && item.items.Count > 0)
-            {
-                element += $"<li>{Converter.ToString(item.content)}</li>";
-            }
+            var content = Converter.ToString(item.content);
+            listHtml.Append($"<li>{content}</li>");
 
-            string nested = string.Empty;
-            if (item.items.Count > 0)
+            if (item.items?.Count > 0)
             {
-                nested = ListMaker(item.items, style);
-            }
-
-            if (!string.IsNullOrEmpty(item.content.ToString()))
-            {
-                element += $"<li>{Converter.ToString(item.content)}</li>" + nested;
+                listHtml.Append(BuildListItems(item.items, listType));
             }
         }
 
-        return $"<{style}>{element}</{style}>";
+        listHtml.Append($"</{listType}>");
+        return listHtml.ToString();
     }
 
     private static string ParseCode(dynamic block)
     {
-        string code = Converter.ToString(block.data.code);
-        return $"<pre class='language-{Converter.ToString(block.data.language)}' lang='{Converter.ToString(block.data.language)}'>" +
-            $"<code class='scrollbar language-{Converter.ToString(block.data.language)}'>" +
-            $"{code.Replace("<", "&lt;")}" +
-            $"</code></pre>";
+        var code = Converter.ToString(block.data.code).Replace("<", "&lt;");
+        var language = Converter.ToString(block.data.language);
+
+        return $"<pre class='language-{language}' lang='{language}'>" +
+               $"<code class='scrollbar language-{language}'>{code}</code></pre>";
     }
 
     private static string ParseQuote(dynamic block)
     {
-        return $"<blockquote>" +
-            $"<p>{Converter.ToString(block.data?.text)}</p>" +
-            $"<cite>{Converter.ToString(block.data?.caption)}</cite>" +
-            $"</blockquote>";
+        var text = Converter.ToString(block.data?.text);
+        var caption = Converter.ToString(block.data?.caption);
+
+        return $"<blockquote><p>{text}</p><cite>{caption}</cite></blockquote>";
     }
 
     private static string ParseLink(dynamic block)
     {
-        return $"<a href='{Converter.ToString(block.data?.link)}' target='_blank'>" +
-            $"{Converter.ToString(block.data?.meta?.title) ?? Converter.ToString(block.data?.link)}" +
-            $"</a>";
+        var link = Converter.ToString(block.data?.link);
+        var title = Converter.ToString(block.data?.meta?.title) ?? link;
+
+        return $"<a href='{link}' target='_blank'>{title}</a>";
     }
 
     private static string ParseDivider()
