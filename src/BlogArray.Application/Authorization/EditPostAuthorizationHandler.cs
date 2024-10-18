1dfc1f9229826587d7117ad1d79a2a3044407c54
Sisindry Medagam <24821639+sisindrymedagam@users.noreply.github.com>
2024-10-18 20:42:05 +0530

    auth entity

--- src/BlogArray.Application/Authorization/EditPostAuthorizationHandler.cs
+++ src/BlogArray.Application/Authorization/EditPostAuthorizationHandler.cs
@@ -1,29 +1,31 @@
-﻿using BlogArray.Domain.Constants;
+﻿using BlogArray.Domain.Constants; 
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
 
 namespace BlogArray.Application.Authorization;
 
 /// <summary>
-/// 
+/// Authorization handler to determine if a user has the permission to edit/delete a blog post.
 /// </summary>
 public class EditPostAuthorizationHandler : AuthorizationHandler<EditPostRequirement, int>
 {
     /// <summary>
-    /// 
+    /// Handles the authorization logic to determine if the current user can edit/delete the post.
     /// </summary>
-    /// <param name="context"></param>
-    /// <param name="requirement"></param>
-    /// <param name="post"></param>
-    /// <returns></returns>
+    /// <param name="context">Authorization handler context that contains the user claims.</param>
+    /// <param name="requirement">The edit post authorization requirement.</param>
+    /// <param name="postAuthorId">The ID of the author who owns the post being edited.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, EditPostRequirement requirement, int postAuthorId)
     {
         var user = context.User;
 
+        // Check if the user has Admin or Editor role, allowing them to edit any post.
         if (user.IsInRole(RoleConstants.Admin) || user.IsInRole(RoleConstants.Editor))
         {
             context.Succeed(requirement); // Admin and Editor can edit all posts
         }
+        // Check if the user is an Author or Contributor and if they own the post.
         else if (user.IsInRole(RoleConstants.Author) || user.IsInRole(RoleConstants.Contributor))
         {
             var userId = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
@@ -38,6 +40,6 @@ public class EditPostAuthorizationHandler : AuthorizationHandler<EditPostRequire
 }
 
 /// <summary>
-/// 
+/// Represents the authorization requirement to edit/delete a post.
 /// </summary>
 public class EditPostRequirement : IAuthorizationRequirement { }
